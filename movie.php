f73914559bb341922191ebd5d8c7b400fbb268bd
Norman <marcus-norman@hotmail.com>
2018-11-07 00:57:02 +0100

    tissemand

--- movie.php
+++ movie.php
@@ -27,7 +27,7 @@
 <meta property="og:image" content="http://www.domain.dk/images/jpg">
 <meta property="og:description" content="Beskrivelse">
 <meta property="og:locale" content="da_DK">
-
+    <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
 <!-- Sikrer man kan benytte CSS ved at tilkoble en CSS fil -->
 <link href="css/styles.css" rel="stylesheet" type="text/css">
 
@@ -38,5 +38,108 @@
 
 <!-- i <body> har man alt indhold på siden -->
 <body>
+<header>
+    <div id="menu">
+        <div class="logo"><p>MWM</p></div>
+
+        <nav id="navWeb">
+            <a href="">Populære film</a>
+            <a href="">Genre</a>
+            <a href="">Bruger</a>
+        </nav>
+        <nav id="navElements">
+            <a href="">POPULÆRE FILM</a>
+            <a href="">GENRE</a>
+            <a href="">BRUGER</a>
+        </nav>
+        <nav id="navMobile">
+            <i id="navBars" class="fas fa-bars fa-2x"></i>
+        </nav>
+    </div>
+</header>
+
+<content>
+    <div class="gridmovies">
+        <div class="movie1">
+            <img src="images/mwm/avatar.jpg" alt="film">
+        </div>
+
+        <div class="movie2">
+            <img src="images/mwm/avenger.jpg" alt="film">
+        </div>
+
+        <div class="movie3">
+            <img src="images/mwm/deadpool.jpg" alt="film">
+        </div>
+
+        <div class="movie4">
+            <img src="images/mwm/game_night.jpg" alt="film">
+        </div>
+
+        <div class="movie5">
+            <img src="images/mwm/incredibles_2.jpg" alt="film">
+        </div>
+
+        <div class="movie6">
+            <img src="images/mwm/interstellar.jpg" alt="film">
+        </div>
+
+        <div class="movie7">
+            <img src="images/mwm/jumanji.jpg" alt="film">
+        </div>
+
+        <div class="movie8">
+            <img src="images/mwm/split.jpg" alt="film">
+        </div>
+
+        <div class="movie9">
+            <img src="images/mwm/titanic.jpg" alt="film">
+        </div>
+
+        <div class="movie10">
+            <img src="images/mwm/the_greatest_showman.jpg" alt="film">
+        </div>
+
+        <div class="movie11">
+            <img src="images/mwm/the_notebook.jpg" alt="film">
+        </div>
+
+        <div class="movie12">
+            <img src="images/mwm/thor_ragnarok.jpg" alt="film">
+        </div>
+
+    </div>
+</content>
+
+
+<footer>
+    <div class="some">
+        <a href="https://www.facebook.com" target="_blank"><i class="fab fa-facebook-square fa-2x"></i></a>
+        <a href="https://www.instagram.com" target="_blank"><i class="fab fa-instagram fa-2x"></i></a>
+        <a href="https://www.linkedin.com" target="_blank"><i class="fab fa-linkedin fa-2x"></i></a>
+    </div>
+</footer>
+
+
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script>
+    $(document).ready(function (e) {
+
+        $("#navMobile").click(function (e) {
+            $("#navElements").slideToggle(300);
+        });
+
+        $(window).resize(function (e) {
+            var width = $(window).width();
+
+            if(width > 768) {
+                $("#navElements").css("display", "none");
+            }
+
+        });
+    });
+</script>
 </body>
 </html>
